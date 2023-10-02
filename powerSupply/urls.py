5185c5044d8fb35a14976d4fc3b39f8f3ea96a30
Akshit Tyagi <AkshitTyagi7>
2023-10-02 15:28:28 +0530

    added name

--- powerSupply/urls.py
+++ powerSupply/urls.py
@@ -20,3 +20,7 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+admin.site.site_header = "Power Supply Adminstration"
+admin.site.site_title = "Power Supply Admin Portal"
+admin.site.index_title = "Welcome to Power Supply Portal"
